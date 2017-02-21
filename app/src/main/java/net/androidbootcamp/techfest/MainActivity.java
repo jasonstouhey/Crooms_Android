d3bec3aa624898554aaeccf6ee622a9878f7c479
Shane Phillips <phillipsshane007@gmail.com>
2017-02-21 18:24:57 -0500

    no message

---
+++ app/src/main/java/net/androidbootcamp/techfest/MainActivity.java
@@ -0,0 +1,24 @@
+package net.androidbootcamp.techfest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageButton button1 = (ImageButton)findViewById(R.id.imgButton1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+    }
+}
